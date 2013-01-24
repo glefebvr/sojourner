3f3ed84f83f0708ef49e62dfc9626ac64ef7b345
Olivier Saut <osaut@airpost.net>
2013-01-24 15:51:30 +0100

    On accélère l'écriture de la grille non structurée (en ne recalculant pas les coins à chaque fois et en ne traitant que les nœuds correspondants aux feuilles)

--- lib/sojourner/write_grid.rb
+++ lib/sojourner/write_grid.rb
@@ -5,8 +5,8 @@ module DiskWriter
       File.open("#{fname}.vtk",File::CREAT | File::TRUNC | File::WRONLY) do |pfi|
         name=File.basename(fname)
 
-        # On considère tous les descendants
-        nodes=root.descendants
+        # On considère les feuilles de l'arbre uniquement
+        nodes=@cached_leaves
 
         # Création de l'entête
         pfi.puts "# vtk DataFile Version 3.0\n#{name}\nASCII\nDATASET UNSTRUCTURED_GRID"
@@ -45,14 +45,20 @@ module DiskWriter
         name=File.basename(fname)
 
         # On considère les feuilles
-        nodes=leaves
+        nodes=@cached_leaves
+
+        coins=[]
+        num_corners=root.bbox.num_corners
+        nodes.each { |n|
+          coins += n.corners
+        }
 
         # Création de l'entête
         pfi.puts "# vtk DataFile Version 3.0\n#{name}\nASCII\nDATASET UNSTRUCTURED_GRID"
-        pfi.puts "POINTS #{vertices.size} float"
+        pfi.puts "POINTS #{coins.size} float"
 
         # Coordonnées des nœuds
-        vertices.each do |vv|
+        coins.each do |vv|
           if root.bbox.size == 2
             pfi.puts "#{vv[0]} #{vv[1]} 0"
           else
@@ -67,7 +73,7 @@ module DiskWriter
         nodes.each do |nn|
           str =  (root.bbox.size==2 ? 4 : 8).to_s
           nn.corners.each do |corner|
-                str << " #{vertices.index(corner)}"
+                str << " #{coins.index(corner)}"
           end
           pfi.puts str
         end
